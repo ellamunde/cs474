4511ea4f23387d091cc03fd8cdcdf3322e216ef1
vika0392 <vika03_92@mail.ru>
2017-11-25 14:51:41 +0900

    Update experiment_ML.py

--- src/experiment_ML.py
+++ src/experiment_ML.py
@@ -10,10 +10,15 @@ neg = "negative"
 neu = "neutral"
 train_a = preprocessing.get_data(train, "A")
 test_a=preprocessing.get_data(test, "A")
-#takes to much time to process limit to 500 instances only
-tokens_pos = preprocessing.get_tokens(train_a[:1000], 'positive', 'CLEANED')
-tokens_neg = preprocessing.get_tokens(train_a[:1000], 'negative', 'CLEANED')
-tokens_neu = preprocessing.get_tokens(train_a[:1000], 'neutral', 'CLEANED')
+#equal number of instances for each class
+pos_set=preprocessing.get_subset(train_a,'positive')
+neg_set=preprocessing.get_subset(train_a,'negative')
+neut_set=preprocessing.get_subset(train_a,'neutral')
+
+#takes too much time to process limit to 500 instances only
+tokens_pos = preprocessing.get_tokens(pos_set[:500])
+tokens_neg = preprocessing.get_tokens(neg_set[:500])
+tokens_neu = preprocessing.get_tokens(neut_set[:500])
 
 tokens_pos=preprocessing.get_tokens_only(tokens_pos)
 tokens_neg=preprocessing.get_tokens_only(tokens_neg)
@@ -23,7 +28,8 @@ print len(tokens_pos), len(tokens_neg),len(tokens_neu)
 
 features=preprocessing.filter_pmi([tokens_pos,tokens_neg,tokens_neu])
 print 'creating feature vectors for training'
-X,Y=featureML.create_X_Y(train_a[:1000],features)
+X,Y=featureML.create_X_Y(train_a[:2000],features)
+
 clf=featureML.train_clf(X,Y)
 
 pred_result=clf.predict(X)
