262ee8f339568099d6e0061c7ee681e6ae9cb814
vika0392 <vika03_92@mail.ru>
2017-12-04 03:25:23 +0900

    Update lda_feature_experiment.py

--- src/task_b/lda_feature_experiment.py
+++ src/task_b/lda_feature_experiment.py
@@ -8,12 +8,10 @@ import logistic_regression as logres
 import lda_pipeline
 
 dataset = 'B'
-train_b = preprocessing.open_preprocess_file('train', dataset)
+train_b = preprocessing.open_preprocess_file('train', dataset)[:100]
 
-# --- lda configurations
-passes = 20
-alpha = 'auto'  # or float number
 
-# --- state random
-numpy.random.random(1)
-lda_pipeline.split_and_train(train_b, logres.default_log_res())
+matrix=train_b[['CLEANED','TOPIC']].values
+polarity=train_b['POLARITY']
+
+lda_pipeline.split_and_train(matrix,polarity,logres.initClassifier())
