1b83d0d70b33b32fc036bad7787f3f49ae27e3da
vika0392 <vika03_92@mail.ru>
2017-12-05 22:27:24 +0900

    Update lda_feature_experiment.py

--- src/task_b/lda_feature_experiment.py
+++ src/task_b/lda_feature_experiment.py
@@ -1,19 +1,25 @@
 # https://link.springer.com/chapter/10.1007%2F978-3-642-13657-3_43
-import src.text_to_vector
-from src.measurements import predict
-import numpy
-import src.preprocessing as preprocessing
-import src.logistic_regression as logres
+from src import measurements
+from src import preprocessing
+from src import logistic_regression as logres
 import lda_pipeline
-
+from pandas import concat
 dataset = 'B'
-train_b = preprocessing.open_preprocess_file('train', dataset)[:100]
-test_b = preprocessing.open_preprocess_file('test', dataset)[:100]
+train_b = preprocessing.open_preprocess_file('train', dataset)
+test_b = preprocessing.open_preprocess_file('test', dataset)
+if isinstance(train_b['POLARITY'][0], basestring):
+    train_b = concat(
+        [train_b[train_b.POLARITY == 'positive'], train_b[train_b.POLARITY == 'negative']]).reset_index(
+        drop=True)
 matrix=train_b[['CLEANED','TOPIC']].values
 polarity=train_b['POLARITY']
 
-model=lda_pipeline.split_and_train(matrix,polarity,logres.default_log_res())
-
+model,lda_pipeline_model=lda_pipeline.split_and_train(matrix,polarity)
+#testing
 matrix=test_b[['CLEANED','TOPIC']].values
 polarity=test_b['POLARITY']
-measurements.predict(matrix,polarity,model)
+prediction=lda_pipeline.predict(model,lda_pipeline_model,matrix,polarity)
+
+measurements.get_accuracy(prediction)
+measurements.avg_recall(prediction)
+
