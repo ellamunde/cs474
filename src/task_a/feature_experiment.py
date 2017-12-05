2132a2172162151d850eb230c87f7d8d4b211e4e
vika0392 <vika03_92@mail.ru>
2017-12-05 22:32:56 +0900

    Update feature_experiment.py

--- src/task_a/feature_experiment.py
+++ src/task_a/feature_experiment.py
@@ -1,62 +1,30 @@
-import preprocessing
-
-from task_a import features
-import measurements
-import logistic_regression_multi as logres
-from sklearn.utils import shuffle
-train = "train"
-test = "test"
-pos = "positive"
-neg = "negative"
-neu = "neutral"
-
+from src import preprocessing
+from src import pre_task_bc_lda as pre
+from src import features
+from src import measurements
 
 train_a = preprocessing.open_preprocess_file('train', 'A')
-# tokens_arr, sents_arr = preprocessing.preprocess(text)
-# train_a = preprocessing.get_data(train, "A")
-
-pos_set=preprocessing.get_subset(train_a,'positive')
-neg_set=preprocessing.get_subset(train_a,'negative')
-neut_set=preprocessing.get_subset(train_a,'neutral')
-# pos_set=preprocessing.get_subset(train_a,'positive')[:500]
-# neg_set=preprocessing.get_subset(train_a,'negative')[:500]
-# neut_set=preprocessing.get_subset(train_a,'neutral')[:500]
-# train_a=pd.concat([pos_set,neg_set,neut_set])
-train_a=shuffle(train_a)
-
 test_a=preprocessing.open_preprocess_file('test', 'A')
-tok_list=preprocessing.get_token_for_each_tweet(train_a)
-tok_list_test=preprocessing.get_token_for_each_tweet(test_a)
+tok_list=preprocessing.get_token_for_each_tweet(train_a['CLEANED'])
+tok_list_test=preprocessing.get_token_for_each_tweet(test_a['CLEANED'])
 #training
-print len(train_a['POLARITY'])
-print len(tok_list)
 X=[]
 Y=train_a['POLARITY']
 for i in tok_list:
     tokens=i[0]
     word_count=i[1]
-    polarity=i[2]
-    
     vec= features.build_feature_vector(tokens, word_count)
-
     X.append(vec)
-#clf=featureML.train_clf(X,Y)
-print len(X)
-print len(Y)
-
-print 'building model'
-clf=logres.split_and_train(X, Y)
-measurements.predict(X,Y,clf)
+train_model =pre.classify('logres',X, Y,multi=True, tuning=False)
 
 X_test=[]
 Y_test=test_a['POLARITY']
 for i in tok_list_test:
     tokens=i[0]
     word_count=i[1]
-    polarity=i[2]
-
     vec= features.build_feature_vector(tokens, word_count)
-
     X_test.append(vec)
 
-measurements.predict(X_test,Y_test,clf)
+prediction = measurements.predict(X_test, Y_test, train_model)
+measurements.get_accuracy(prediction)
+measurements.avg_recall(prediction)
